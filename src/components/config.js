c65dae96bd2e3226677de63a512010a2a78b255b
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-04-25 10:34:26 +0100

    api_url

--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,6 @@
-const ENV = "development"; // Change to 'production' in live environment
+const ENV = "production"; 
 
 const LOCAL_API = "http://127.0.0.1:8000";
 const LIVE_API = "https://ayo.newstropy.online";
 
-export const API_URL = ENV === "production" ? LIVE_API : LOCAL_API;
+export const API_URL = ENV === "development" ? LOCAL_API : LIVE_API;
