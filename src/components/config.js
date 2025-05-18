eb3fdc1552126d7c7f2e928dfe05f1fde9ad9f35
oluwamomiAkinbote <oluwamomis@gmail.com>
2025-05-18 14:19:18 +0100

    api change

--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,6 @@
 const ENV = import.meta.env.MODE || "production"; 
 
 const LOCAL_API = "http://127.0.0.1:8000";
-const LIVE_API = "https://ayo.newstropy.online";
+const LIVE_API = "https://ayo.newsstropy.online";
 
 export const API_URL = ENV === "development" ? LOCAL_API : LIVE_API;
